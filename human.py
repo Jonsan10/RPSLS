394ccc08d9843177bb9571ef1e0e118fb6926cf0
jonathan sanchez <sanchezjon480@gmail.com>
2022-07-05 13:17:18 -0700

    updated files

--- human.py
+++ human.py
@@ -1,4 +1,9 @@
+from player import Player
 #child
 class Human(Player):
     def __init__(self,name, win_counter):
         super().__init__(name, win_counter)
+
+    def choose_gesture(self):
+        print('rock, paper, scissors, lizards, spock')
+
