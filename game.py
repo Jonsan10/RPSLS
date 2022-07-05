4534f95d03f276501f3aca4c6d41a839a5a2846c
jonathan sanchez <sanchezjon480@gmail.com>
2022-07-05 11:50:07 -0700

    updated code

---
+++ game.py
@@ -0,0 +1,22 @@
+class Game:
+    def __init__(self):
+        self.grettings()
+        self.rules()
+        self.game_mode()
+        self.run_game()
+        self.display_winner()
+
+    def greetings(self):
+        print('Welcome to RPSLS')
+
+    def rules(self):
+        print('Rules of the game are, Scissors cuts Paper,')
+
+    def game_mode(self):
+        pass
+
+    def run_game(self):
+        pass
+
+    def display_winner(self):
+        pass
---
+++ human.py
@@ -0,0 +1,6 @@
+class player:
+    def __init__(self,name, win_counter):
+        self.name = name
+        self.gesters = ''
+        self.win_counter = win_counter
+
