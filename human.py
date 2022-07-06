594606b66b6c0fc1583c23ce275f8b88a1bdc047
Hot Mama <hotmama@Hots-MBP.hsd1.or.comcast.net>
2022-07-06 10:09:44 -0700

    worked on creating Human class

--- human.py
+++ human.py
@@ -1,9 +1,21 @@
 from player import Player
 #child
 class Human(Player):
-    def __init__(self,name, win_counter):
-        super().__init__(name, win_counter)
+    def __init__(self):
+        super().__init__()
         
 def choose_gesture (self):
-            print('rock, paper, scissors, lizards, spock')
+    # present gesture options to user
+    # capture user's selection
+    # index the gestures list with the user's selection
+    # display user's selection to user
 
+    # (validate the user's selection)
+
+    for num in range(0, len(self.gestures_list)):
+        print(f'{num} {self.gestures_list[num]}')
+
+    user_selection = int(input('Please use the number keys to enter your gesture selection:  '))
+    self.chosen_gesture = self.gestures_list[user_selection]
+    print(f'Nicely done {self.name}, you have selected {self.chosen_gesture}')
+           
--- user stories.md
+++ user stories.md
@@ -1,6 +1,6 @@
 (5 points): As a developer, I want to make at least 10 commits with descriptive messages.
 
-(15 points): As a developer, I want to find a way to properly incorporate inheritance into my game.
+<!-- (15 points): As a developer, I want to find a way to properly incorporate inheritance into my game. -->
 
 (5 points): As a developer, I want to account for and handle bad user input, ensuring that any user input is validated and reobtained if necessary.
 
